5a85cf37fe65a887776d57095a149149f2a0fa45
Christopher Schiefelbein <ch.schiefelbein@duocuc.cl>
2025-10-16 15:10:22 +0000

    Modificacion del archivo index.html y App.jsx para actualizar el título y el contenido de la aplicación.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 // src/App.jsx
 // Configuración de rutas principales con React Router
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// Usamos HashRouter en lugar de BrowserRouter para compatibilidad con GitHub Pages,
+// ya que GitHub Pages no soporta rutas basadas en el historial del navegador.
+// HashRouter utiliza rutas basadas en el hash (#), lo que puede afectar el SEO y la apariencia de la URL.
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Eventos from './pages/Eventos';
 import Auth from "./pages/Auth.jsx";
@@ -11,7 +14,7 @@ import Perfil from "./pages/Perfil.jsx";
 
 function App() {
     return (
-        <BrowserRouter>
+        <Router>
             <Routes>
                 {/* Ruta principal - Home */}
                 <Route path="/" element={<Home />} />
@@ -22,7 +25,7 @@ function App() {
                 <Route path="/admin" element={<ProtectedRoute><Admin /></ProtectedRoute>}/>
                 <Route path="/perfil" element={<ProtectedRoute><Perfil /></ProtectedRoute>}/>
             </Routes>
-        </BrowserRouter>
+        </Router>
     );
 }
 
