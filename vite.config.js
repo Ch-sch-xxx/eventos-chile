c57d908a66931ee3f6fb28b2f59261c15e9d1a64
ignacioruizberenguela <ig.ruizb@duocuc.cl>
2025-10-16 11:53:07 -0300

    a
    a

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// Configuración de Vitest
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom', // Simula navegador
+    globals: true,         // Permite usar describe/it/expect sin importar
+    setupFiles: './src/setupTests.js' // Archivo de configuración adicional
+  }
 })
