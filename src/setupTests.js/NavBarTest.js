426f62fdd66c5017c57797b569a5d386ce476edb
ignacioruizberenguela <ig.ruizb@duocuc.cl>
2025-10-16 12:00:48 -0300

    a
    a

--- src/setupTests.js/NavBarTest.js
+++ src/setupTests.js/NavBarTest.js
@@ -1,3 +1,3 @@
-import { describe, it, expect, beforeEach, vi } from "vitest";
-import { obtenerEventos, guardarEventos, crearEvento, listarEventos } from "./eventosStorage";
 
+import { describe, it, expect } from 'vitest'
+import Navbar from './Navbar'
