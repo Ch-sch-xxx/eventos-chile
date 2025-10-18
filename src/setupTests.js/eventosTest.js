25acfbd0984ac851ee86b2cea4670ddf2360d2e9
ignacioruizberenguela <ig.ruizb@duocuc.cl>
2025-10-18 12:37:07 -0300

    eventos simulados

--- src/setupTests.js/eventosTest.js
+++ src/setupTests.js/eventosTest.js
@@ -1,2 +1,18 @@
 import { describe, it, expect, beforeEach, vi } from "vitest";
 import {obtenerEventos,guardarEventos,crearEvento,editarEvento,eliminarEvento,obtenerEventosPorUsuario,contarEventosUsuario,contarTotalUsuarios,obtenerMisEventos} from "../eventosStorage";
+
+
+  const eventosIniciales = [
+    { id: "1", titulo: "Evento 1", creadoPor: "admin@chile.cl" },
+    { id: "2", titulo: "Evento 2", creadoPor: "usuario@chile.cl" },
+  ];
+
+  localStorage.setItem("eventos-chile", JSON.stringify(eventosIniciales));
+  localStorage.setItem("usuarios-chile", JSON.stringify([
+    { email: "admin@chile.cl" },
+    { email: "usuario@chile.cl" }
+  ]))
+
+  global.STORAGE_KEY = "eventos-chile";
+  global.eventosIniciales = eventosIniciales;
+});
