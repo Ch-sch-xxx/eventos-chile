053c30fd6b48851ba8293f5a3701b0ce85798af4
Christopher Schiefelbein <ch.schiefelbein@duocuc.cl>
2025-10-19 10:37:26 -0300

    Revert "Mejora configuración para GitHub Pages"
    This reverts commit aec95af7afd9943b5654034017cf134c39e533c2.

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/eventos-chile/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true
-  }
+base: '/eventos-chile/' ,
 })
